75020e4447482acd3ef0d2f65450bb89f6cf5c9d
Alexander <aaoyarzabal@gmail.com>
2017-04-30 11:19:53 -0400

    test with data

--- app.py
+++ app.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 import warnings
 from matplotlib import style 
 from collections import Counter
+import pandas as pd 
+import random 
 
-style.use('fivethirtyeight')
+#style.use('fivethirtyeight')
 
-dataset = {'k':[[1,2],[2,3],[3,1]],'r':[[6,5],[7,7],[8,6]]}
-new_features = [5,7]
+#dataset = {'k':[[1,2],[2,3],[3,1]],'r':[[6,5],[7,7],[8,6]]}
+#new_features = [5,7]
 
 def k_nearest_neighbors(data, predict, k=3):
 	if len(data) >= k:
@@ -23,16 +25,48 @@ def k_nearest_neighbors(data, predict, k=3):
 	vote_result = Counter(votes).most_common(1)[0][0]
 	return vote_result
 
-result = k_nearest_neighbors(dataset, new_features, k=3)
-print(result)
+df = pd.read_csv("breast-cancer-wisconsin.data.txt")
+df.replace('?',-99999,inplace=True)
+df.drop(['id'],1,inplace=True)
+print(df.head())
+full_data = df.astype(float).values.tolist()
+print(full_data[:5])
+random.shuffle(full_data)
+print(20*'#')
+print(full_data[:5])
 
-[[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]] for i in dataset]
-plt.scatter(new_features[0],new_features[1], color = result)
-plt.show()
+test_size = 0.2
+train_set = {2:[], 4:[]}
+test_set = {2:[],4:[]}
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
 
-#Test data
-plot1 = [1,3]
-plot2 = [2,5]
+for i in train_data:
+	train_set[i[-1]].append(i[:-1])
+for i in test_data:
+	test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+	for data in test_set[group]:
+		vote = k_nearest_neighbors(train_set,data,k=5)
+		if group == vote:
+			correct +=1
+		total += 1
+print('Accuracy: ', correct/total)
+
+# result = k_nearest_neighbors(dataset, new_features, k=3)
+# print(result)
+
+# [[plt.scatter(ii[0],ii[1],s=100,color=i) for ii in dataset[i]] for i in dataset]
+# plt.scatter(new_features[0],new_features[1], color = result)
+# plt.show()
+
+# #Test data
+# plot1 = [1,3]
+# plot2 = [2,5]
 
 #Example
 #euclidean_distance = sqrt((plot1[0]-plot2[0])**2 + (plot1[1]-plot2[1])**2 )
